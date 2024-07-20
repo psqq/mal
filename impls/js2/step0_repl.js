f459e415d36fda227183fc6c5e872bd63efe2edc
Sergey Pestov <pestov-sa@ya.ru>
2024-07-20 23:29:05 +0300

    js2 step 0 done

---
+++ impls/js2/step0_repl.js
@@ -0,0 +1,28 @@
+import { createInterface } from 'readline';
+
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+}).on('close', () => process.exit(0));
+
+async function READ() {
+    return new Promise((resolve) => {
+        rl.question('user> ', resolve);
+    });
+}
+
+function EVAL(s) {
+    return s;
+}
+
+function PRINT(s) {
+    console.log(s);
+}
+
+async function rep() {
+    const inp = await READ();
+    const result = EVAL(inp);
+    PRINT(result);
+}
+
+while(true) await rep();
